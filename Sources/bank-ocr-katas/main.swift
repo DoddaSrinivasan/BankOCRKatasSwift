014e9787f4c1ac9584111568517567b591521c0f
Srinivasan Dodda <srinivasan.munna@gmail.com>
2020-11-16 16:48:51 +0100

    linting

--- Sources/bank-ocr-katas/main.swift
+++ Sources/bank-ocr-katas/main.swift
@@ -4,17 +4,17 @@ import ArgumentParser
 struct OCRFileParser: ParsableCommand {
     
     static let configuration = CommandConfiguration(
-            abstract: "A Swift command-line tool to read ocr file",
-            subcommands: [])
-  
-  @Argument(help: "ocr file to parse")
-  var file: String
-  
-  func run() throws {
-    let contents = try String(contentsOfFile: file)
-    let numbers = MultiLineSevenSegmentParser().numbers(from: contents)
-    numbers.forEach { print($0.description) }
-  }
+        abstract: "A Swift command-line tool to read ocr file",
+        subcommands: [])
+    
+    @Argument(help: "ocr file to parse")
+    var file: String
+    
+    func run() throws {
+        let contents = try String(contentsOfFile: file)
+        let numbers = MultiLineSevenSegmentParser().numbers(from: contents)
+        numbers.forEach { print($0.description) }
+    }
 }
 
 OCRFileParser.main()
